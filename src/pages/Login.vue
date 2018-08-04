f9879a68a7cde1da0b2048c85299f69f6b0bdb9d
shirin <eghrarish@gmail.com>
2018-08-04 17:55:51 +0430

    Worked on login page style, #7

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -5,15 +5,33 @@
         <img src="./../assets/abstract-logo.png" alt="Maestro" class="maestro">
         <p class="title">Maestro</p>
       </div>
-      <div class="form">
-        form
+      <div class="information">
+        <form class="form">
+          <input type="text" placeholder="  Username or Email (Case sensitive)" class="inputs">
+          <input type="password" placeholder="  Password (Case sensitive)" class="inputs">
+          <router-link to="" class="">Forgot Password?</router-link>
+          <p class="exception">Lorem ipsum dolor sit amet, consectetur </p>
+          <button type="submit" class="button">Log in</button>
+        </form>
       </div>
       <div class="login-with">
-        login with
+        <p class="title">Or login with</p>
+        <div class="icons">
+          <img src="./../assets/Carrene.logo.png" alt="Carrene" class="img">
+          <p>Carrene</p>
+        </div>
+        <div class="icons">
+          <img src="./../assets/Google.logo.png" alt="Google" class="img">
+          <p>Google</p>
+        </div>
+        <div class="icons">
+          <img src="./../assets/Github.logo.png" alt="Github" class="img">
+          <p>Github</p>
+        </div>
       </div>
-      <div class="register">
-        register
-      </div>
+        <p class="register">
+          Not registered yet? create account <router-link to="" class="">here</router-link>
+        </p>
      </div>
   </div>
 </template>
