78c4401000b9350a3eb0aeb79effc1215d21408e
forough-jalili <foroughjalili@gmail.com>
2018-08-05 18:10:12 +0430

    Refactored codes and changed design and added Vtooltip package, #6

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VTooltip from 'v-tooltip'
 
 Vue.config.productionTip = false
 
 require('./styles/index.sass')
+
+Vue.use(VTooltip)
 
 /* eslint-disable no-new */
 new Vue({
