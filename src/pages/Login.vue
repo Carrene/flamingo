7b600a8131d5cc652cd11efc4af7dcb413c6d2d0
shirin <eghrarish@gmail.com>
2018-08-05 18:20:59 +0430

    Updated login page layout, #7

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,36 +1,40 @@
 <template>
   <div class="login">
     <div class="body">
-      <div class="icon">
-        <img src="./../assets/abstract-logo.png" alt="Maestro" class="maestro">
-        <p class="title">Maestro</p>
+      <div class="logo">
+        <img src="./../assets/abstract-logo.png" alt="Maestro" class="logo-img">
+        <p class="logo-title">Maestro</p>
       </div>
       <div class="information">
         <form class="form">
-          <input type="text" placeholder="  Username or Email (Case sensitive)" class="inputs">
-          <input type="password" placeholder="  Password (Case sensitive)" class="inputs">
-          <router-link to="" class="">Forgot Password?</router-link>
-          <p class="exception">Lorem ipsum dolor sit amet, consectetur </p>
-          <button type="submit" class="button">Log in</button>
+          <input type="text" placeholder="Username (Case sensitive)" class="email input">
+          <div class="password-container">
+            <input type="password" placeholder="Password (Case sensitive)" class="password input">
+            <img src="./../assets/Hide password.png" class="visible-icon">
+          </div>
+          <div class="clickable">
+            <router-link to="" class="link">Forgot Password?</router-link>
+            <button type="submit" class="button">Log in</button>
+          </div>
         </form>
       </div>
-      <div class="login-with">
+      <div class="login-mode">
         <p class="title">Or login with</p>
-        <div class="icons">
-          <img src="./../assets/Carrene.logo.png" alt="Carrene" class="img">
+        <div class="login-mode-icons">
+          <img src="./../assets/Carrene.logo.png" alt="Carrene" class="icon">
           <p>Carrene</p>
         </div>
-        <div class="icons">
-          <img src="./../assets/Google.logo.png" alt="Google" class="img">
+        <div class="login-mode-icons">
+          <img src="./../assets/Google.logo.png" alt="Google" class="icon">
           <p>Google</p>
         </div>
-        <div class="icons">
-          <img src="./../assets/Github.logo.png" alt="Github" class="img">
+        <div class="login-mode-icons">
+          <img src="./../assets/Github.logo.png" alt="Github" class="icon">
           <p>Github</p>
         </div>
       </div>
         <p class="register">
-          Not registered yet? create account <router-link to="" class="">here</router-link>
+          Not registered yet? create account <router-link to="" class="link">here</router-link>
         </p>
      </div>
   </div>
