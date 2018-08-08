dd45840039d21b9a53587c16b41294249c55fc8d
shirin <eghrarish@gmail.com>
2018-08-08 16:06:45 +0430

    Applied requests change, #7

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -5,30 +5,55 @@
         <img src="./../assets/abstract-logo.png" alt="Maestro" class="logo-img">
         <p class="logo-title">Maestro</p>
       </div>
-      <form @submit.prevent="login" class="form">
+      <form @submit.prevent="login" class="information">
         <div class="username-container">
-          <input type="text" placeholder="Username (Case sensitive)" class="email input"
-                 :class="$v.username.$error ? 'error' : null"
-                 v-model="$v.username.$model"
+          <input
+            type="text"
+            placeholder="Username (Case sensitive)"
+            class="email input"
+            :class="$v.credentials.username.$error ? 'error' : null"
+            v-model="credentials.username"
+            @blur="$v.credentials.username.$touch"
+            @focus="$v.credentials.username.$reset"
           >
-          <p class="validation-error" v-if="$v.username.$error">This value is required.</p>
+          <div class="validation-error" v-if="$v.credentials.username.$error">
+            <p v-if="!$v.credentials.username.required">This value is required.</p>
+          </div>
         </div>
         <div class="password-container">
-          <div class="container">
-            <input placeholder="Password (Case sensitive)" class="password input"
-                   :type="passwordFieldType"
-                   :class="$v.password.$error ? 'error' : null" v-model="$v.password.$model"
+          <div class="password-field">
+            <input
+              placeholder="Password (Case sensitive)"
+              class="password input"
+              :type="passwordFieldType"
+              :class="$v.credentials.password.$error ? 'error' : null"
+              v-model="credentials.password"
+              @blur="$v.credentials.password.$touch"
+              @focus="$v.credentials.password.$reset"
             >
-            <img src="../assets/Hide password.png" class="visible-icon" @click="switchVisibility">
+            <img
+              src="../assets/View.svg"
+              v-if="passwordFieldType === 'password'"
+              class="visible-icon"
+              @click="toggleVisibility"
+            >
+            <img
+              src="../assets/View off.svg"
+              v-if="passwordFieldType === 'text'"
+              class="visible-icon"
+              @click="toggleVisibility"
+            >
           </div>
-          <div class="validation-error">
-            <p v-if="$v.password.$error">This value is required.</p>
+          <div class="validation-error" v-if="$v.credentials.password.$error">
+            <p v-if="!$v.credentials.password.required">This value is required.</p>
           </div>
         </div>
-        <div class="link-button">
-          <router-link to="" class="link">Forgot Password?</router-link>
-          <button type="submit" class="button"
-                  :disabled="$v.username.$invalid || $v.password.$invalid"
+        <div class="actions">
+          <router-link to="forget_password" class="link">Forgot Password?</router-link>
+          <button
+            type="submit"
+            class="login-button"
+            :disabled="$v.credentials.$error"
           >
             Log in
           </button>
@@ -37,55 +62,58 @@
       <div class="login-mode">
         <p class="title">Or login with</p>
         <div class="login-mode-icons">
-          <img src="./../assets/Carrene.logo.png" alt="Carrene" class="icon">
+          <img src="./../assets/Carrene.logo.png" alt="Carrene" class="carrene-icon">
           <p>Carrene</p>
         </div>
         <div class="login-mode-icons">
-          <img src="./../assets/Google.logo.png" alt="Google" class="icon">
+          <img src="./../assets/Google.logo.png" alt="Google" class="google-icon">
           <p>Google</p>
         </div>
         <div class="login-mode-icons">
-          <img src="./../assets/Github.logo.png" alt="Github" class="icon">
+          <img src="./../assets/Github.logo.png" alt="Github" class="github-icon">
           <p>Github</p>
         </div>
       </div>
-        <p class="register">
-          Not registered yet? create account
-          <router-link to="" class="link">here</router-link>
-        </p>
+      <p class="register">
+        Not registered yet? create account
+        <router-link to="signup" class="link">here</router-link>
+      </p>
      </div>
-    <div class="response-error" v-if="showBox === true">
-      <div class="box">
+    <div class="response-error" v-if="showErrorBox === true">
         <p>Oops, something happened ...</p>
-        <img src="./../assets/close.svg" alt="close" class="close-icon" @click="showBox = false">
-        <p class="error"></p>
-      </div>
+        <img src="./../assets/close.svg" alt="close" class="close-icon" @click="showErrorBox = false">
+        <p class="error">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
     </div>
   </div>
 </template>
 
-<script>import { required } from 'vuelidate/lib/validators'
+<script>
+import { required } from 'vuelidate/lib/validators'
 
 export default {
   name: 'Login',
   data () {
     return {
-      username: null,
-      password: null,
-      showBox: false,
+      credentials: {
+        username: null,
+        password: null
+      },
+      showErrorBox: false,
       passwordFieldType: 'password'
     }
   },
   validations: {
-    username: {
-      required
-    },
-    password: {
-      required
+    credentials: {
+      username: {
+        required
+      },
+      password: {
+        required
+      }
     }
   },
   methods: {
-    switchVisibility () {
+    toggleVisibility () {
       this.passwordFieldType = this.passwordFieldType === 'password' ? 'text' : 'password'
     },
     login () {
