65f7c9eb2ee82f019eb7dba54e3b3923f61cbe79
shirin <eghrarish@gmail.com>
2018-08-14 20:00:27 +0430

    Added icons and worked on layout, #13

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,34 +1,13 @@
 <template>
   <div class="header">
-    <div class="color-line">
-      <div class="orange"></div>
-      <div class="purple"></div>
-      <div class="yellow"></div>
-      <div class="blue"></div>
-      <div class="red"></div>
-      <div class="green"></div>
-      <div class="black"></div>
+    <div class="logo"><img src="../assets/MaestroLogo.svg" class="logo-icon"/></div>
+    <div class="project"><p>Project</p></div>
+    <div class="search">
+      <img src="../assets/Search.svg" class="search-icon"/>
+      <input type="text" placeholder="search" class="input">
     </div>
-    <div class="toolbar">
-      <div class="logo">
-        <img src="../assets/Logo.png" class="logo-img"/>
-        <p>maestro</p>
-      </div>
-      <div class="search">
-        <img src="../assets/Search.svg" class="search-icon"/>
-        <p>Search</p>
-      </div>
-      <div class="menu">
-        <img src="../assets/Dashboard.svg" class="dashboard icon"/>
-        <img src="../assets/Chat.svg" class="chat icon"/>
-        <img src="../assets/NotificationIcon.svg" class="notification icon"/>
-      </div>
-      <div class="profile">
-        <p class="username">Username</p>
-        <img src="../assets/ChevronDown.svg" class="chevron-down"/>
-        <img src="../assets/ProfileAvatar.svg" class="profile-pic"/>
-      </div>
-    </div>
+    <div class="notification"><img src="../assets/NotificationIcon.svg"/> </div>
+    <div class="profile">profile</div>
   </div>
 </template>
 
