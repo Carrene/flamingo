06a6d3907d08a659093ee09083131f3037ae55a5
forough-jalili <foroughjalili@gmail.com>
2018-08-15 14:32:22 +0430

    Removing Vtooltip from project

--- src/main.js
+++ src/main.js
@@ -3,14 +3,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuelidate from 'vuelidate'
-import VTooltip from 'v-tooltip'
 
 Vue.config.productionTip = false
 
 require('./styles/index.sass')
-Vue.use(Vuelidate)
-
-Vue.use(VTooltip)
 Vue.use(Vuelidate)
 
 /* eslint-disable no-new */
