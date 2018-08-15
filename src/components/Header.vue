ee4d50a9605a6ef20544b9871bb12f61669bd657
shirin <eghrarish@gmail.com>
2018-08-15 14:58:38 +0430

    Added profile section and updated icons, #13

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="header">
-    <div class="logo"><img src="../assets/MaestroLogo.svg" class="logo-icon"/></div>
-    <div class="project"><p>Project</p></div>
+    <div class="logo">
+      <img src="../assets/MaestroLogo.svg" class="logo-icon"/>
+    </div>
+    <div class="project"><p>{{ entity }}</p></div>
     <div class="search">
-      <img src="../assets/Search.svg" class="search-icon"/>
+      <img src="../assets/search-icon.svg" class="search-icon"/>
       <input type="text" placeholder="search" class="input">
     </div>
-    <div class="notification"><img src="../assets/NotificationIcon.svg"/> </div>
-    <div class="profile">profile</div>
+    <div class="notification">
+      <img src="../assets/NotificationIcon.svg" class="notification-icon"/>
+      <img src="../assets/notification-circle.svg" class="circle" v-if="notification"/>
+    </div>
+    <div class="profile">
+      <p class="username">{{ username }}</p>
+      <div class="profile-pic">
+        <img src="../assets/default-profile-image.svg" class="pic "/>
+        <img src="../assets/admin-icon-shield-group.svg" class="role-icon" v-if="role === 'admin'"/>
+        <img src="../assets/manager-icon-tie-group.svg" class="role-icon" v-if="role === 'manager'"/>
+        <img src="../assets/guest-icon-eye-group.svg" class="role-icon" v-if="role === 'guest'"/>
+        <img src="../assets/resource-icon-device-group.svg" class="role-icon" v-if="role === 'resource'"/>
+      </div>
+      <p class="role">{{ role }}</p>
+    </div>
   </div>
 </template>
 
@@ -17,6 +32,10 @@ export default {
   name: 'Header',
   data () {
     return {
+      notification: false,
+      username: 'shirin',
+      role: 'admin',
+      entity: 'Project'
     }
   }
 }
