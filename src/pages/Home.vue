7dd7cbf9b577a3cf0fbfbd6f0ce3e8eaf53a5a56
shirin <eghrarish@gmail.com>
2018-08-18 12:27:29 +0430

    Worked on functionality, #17

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -17,13 +17,41 @@
 </template>
 
 <script>
+import server from './../server'
+
 export default {
   name: 'Home',
   data () {
     return {
       projectName: 'alpha',
+      projectList: null,
+      issuesList: null,
       unreadMessage: '999'
     }
+  },
+  methods: {
+    listProjects () {
+      server
+        .request('projects')
+        .setVerb('LIST')
+        .send()
+        .then(resp => {
+          this.projectList = resp.json
+        })
+    },
+    listIssues () {
+      server
+        .request('issues')
+        .setVerb('List')
+        .send()
+        .then(resp => {
+          this.issuesList = resp.json
+        })
+    }
+  },
+  mounted () {
+    this.listProjects()
+    this.listIssues()
   }
 }
 </script>
