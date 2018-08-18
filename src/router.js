71d1e43ef59734eb0908db122831b18501a260e2
forough-jalili <foroughjalili@gmail.com>
2018-08-18 10:22:25 +0430

    Updated helloword to home page, #18

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/pages/Login'
 import Signup from '@/pages/Signup'
+import Home from '@/pages/Home'
 
 Vue.use(Router)
 
@@ -24,6 +25,11 @@ export default new Router({
       meta: {
         title: Signup
       }
+    },
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
     }
   ]
 })
