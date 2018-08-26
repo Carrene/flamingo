130309eae31019cb8d3b2f7b036234eedc393781
mrastiak <armin.ayari@gmail.com>
2018-08-26 12:47:48 +0430

    Sidebar commits

---
+++ src/components/SideBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="side-bar">
+    <div v-for="item in sideBarItems"
+         :class="[item.name, {selected: item.name === selectedItem}]"
+         :key="item.name"
+         @click="selectedItem = item.name"
+    >
+      <img class="icon" :src="item.iconPath"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SideBar',
+  data () {
+    return {
+      selectedItem: 'projects',
+      sideBarItems: [
+        {
+          name: 'projects',
+          iconPath: require('./../assets/project-icon.svg')
+        },
+        {
+          name: 'issues',
+          iconPath: require('./../assets/issue-icon.svg')
+        }
+      ]
+    }
+  }
+}
+</script>
