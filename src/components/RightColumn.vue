c593bb382fe0886c38732cbeb6a980cecbcfe1bd
shirin <eghrarish@gmail.com>
2018-08-28 17:36:06 +0430

    Convert left column and right column to component, #17, #20

---
+++ src/components/RightColumn.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="right-column">
+    <div v-if="selectedProject" class="project-information">
+      <img src="../assets/description-icon.svg" class="information-icon">
+      <p class="title">Description</p>
+      <p class="text">{{ selectedProject.description }}</p>
+      <img src="../assets/due-date-icon.svg" class="information-icon">
+      <div class="due-date">
+        <p>Due-Date</p>
+        <p class="date">{{ moment(selectedProject.dueDate).format('DD/MM/YYYY') }}</p>
+      </div>
+      <img src="../assets/create-at-icon.svg" class="information-icon">
+      <div class="create-at">
+        <p>Create At</p>
+        <p class="date">{{ moment(selectedProject.createdAt).format('DD/MM/YYYY') }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import moment from 'moment'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  },
+  watch: {
+    'selectedProjectId' (newValue) {
+      this.listIssues()
+    }
+  },
+  computed: mapState([
+    'selectedProject'
+  ]),
+  methods: {
+    moment: moment
+  }
+}
+</script>
