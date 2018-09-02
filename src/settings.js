87987d7e4598abb4de8aad6e320675678b3a2da3
mrastiak <armin.ayari@gmail.com>
2018-09-02 11:33:39 +0430

    Removed port from BASE_URL

--- src/settings.js
+++ src/settings.js
@@ -1,2 +1,2 @@
 export const BASE_URL = process.env.NODE_ENV === 'production'
-  ? `${window.location.protocol}//${window.location.hostname}:${window.location.port}` : 'http://localhost:8081'
+  ? `${window.location.protocol}//${window.location.hostname}` : 'http://localhost:8081'
