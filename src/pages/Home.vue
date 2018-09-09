5b7452fdd51a547764d974845c0455e19d05c018
shirin <eghrarish@gmail.com>
2018-09-09 17:50:58 +0430

    Added chat box and worked on chat box style, #28

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,22 @@
 <template>
   <div id="home" v-if="viewMode === 'chat'">
     <home-left-column />
-    <div class="chat"></div>
+    <div class="chat-container">
+      <chat class="chat" />
+    </div>
     <home-right-column />
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import HomeLeftColumn from '../components/HomeLeftColumn'
 import HomeRightColumn from '../components/HomeRightColumn'
+import Components from '@carrene/vue-chatbox'
 import { mapMutations, mapState } from 'vuex'
+Object.entries(Components).forEach((name, component) => {
+  Vue.component(name, component)
+})
 
 export default {
   name: 'Home',
@@ -30,7 +37,8 @@ export default {
   },
   components: {
     HomeLeftColumn,
-    HomeRightColumn
+    HomeRightColumn,
+    ...Components
   }
 }
 </script>
