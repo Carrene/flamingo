8dc984e5fa09d9ca153fb16e16f22f3e5c7ba8d8
shirin <eghrarish@gmail.com>
2018-09-09 16:39:01 +0430

    Worked on search section style, #31

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,6 +7,27 @@
       <div :class="notification ? 'notification' : null">
         <img src="../assets/notification-icon.svg" class="notification-icon"/>
       </div>
+      <div class="search-result">
+        <div class="tab">
+          <p @click="selectTab" :class="selectedProjectTab ? 'select' : null">project</p>
+          <p @click="selectTab" :class="selectedChatTab ? 'select' : null">chat</p>
+        </div>
+        <div class="result-list" v-if="selectedProjectTab">
+          <div class="result">
+            <div class="place">Status</div>
+            <div class="project-name">Maestro</div>
+            <div class="details">Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
+          </div>
+          <div class="result">
+            <div class="place">Status</div>
+            <div class="project-name">Maestro</div>
+            <div class="details">Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
+          </div>
+        </div>
+        <div class="result" v-if="selectedChatTab">
+          chat
+        </div>
+      </div>
     </div>
     <div class="profile">
       <p class="username">{{ username }}</p>
@@ -28,12 +49,20 @@ export default {
   name: 'Header',
   data () {
     return {
+      selectedProjectTab: true,
+      selectedChatTab: false,
       // TODO: Change all data to dynamic
       authenticated: true,
       notification: true,
       username: 'Username',
       role: 'admin',
       entity: 'Projects'
+    }
+  },
+  methods: {
+    selectTab () {
+      this.selectedProjectTab = !this.selectedProjectTab
+      this.selectedChatTab = !this.selectedChatTab
     }
   }
 }
