319bde5ccfe1f9647b45c3fc2838aca55ba5038a
mrastiak <armin.ayari@gmail.com>
2018-09-10 16:44:24 +0430

    Fixed some ui issues in search result, #31

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,17 +2,17 @@
   <div id="header" v-if="authenticated">
     <div class="entity"><p>{{ entity }}</p></div>
     <div class="search">
-      <img src="../assets/search-icon.svg" class="search-icon" @click="searchResultVisibility"/>
+      <img src="../assets/search-icon.svg" class="search-icon" @click="showSearchResult = !showSearchResult"/>
       <input type="text" placeholder="SEARCH" class="primary-input">
       <div :class="notification ? 'notification' : null">
         <img src="../assets/notification-icon.svg" class="notification-icon"/>
       </div>
       <div class="search-result" v-if="showSearchResult">
         <div class="tabs">
-          <p @click="selectProjectTab" :class="selectedProjectTab ? 'select' : null">project</p>
-          <p @click="selectChatTab" :class="selectedChatTab ? 'select' : null">chat</p>
+          <p @click="resultTab = 'projects'" :class="{select: resultTab === 'projects'}">project</p>
+          <p @click="resultTab = 'chats'" :class="{select: resultTab === 'chats'}">chat</p>
         </div>
-        <div class="result-list" v-if="selectedProjectTab">
+        <div class="result-list" v-if="resultTab === 'projects'">
           <div class="result">
             <div class="field">Status</div>
             <div class="project-name">Maestro</div>
@@ -24,7 +24,8 @@
             <div class="details">Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
           </div>
         </div>
-        <div class="result" v-if="selectedChatTab">
+        <div class="result-list" v-if="resultTab === 'chats'">
+          <div class="result"></div>
         </div>
       </div>
     </div>
@@ -48,28 +49,14 @@ export default {
   name: 'Header',
   data () {
     return {
-      selectedProjectTab: true,
-      selectedChatTab: false,
       showSearchResult: false,
+      resultTab: 'projects',
       // TODO: Change below data to dynamic
       authenticated: true,
       notification: true,
       username: 'Username',
       role: 'admin',
       entity: 'Projects'
-    }
-  },
-  methods: {
-    selectProjectTab () {
-      this.selectedProjectTab = true
-      this.selectedChatTab = false
-    },
-    selectChatTab () {
-      this.selectedProjectTab = false
-      this.selectedChatTab = true
-    },
-    searchResultVisibility () {
-      this.showSearchResult = !this.showSearchResult
     }
   }
 }
