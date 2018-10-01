9cac069e9a52550e85dbe0f817593202450f46bb
shirin <eghrarish@gmail.com>
2018-10-01 13:56:44 +0330

    Finished filter section style, #56

--- src/components/HomeLeftColumn.vue
+++ src/components/HomeLeftColumn.vue
@@ -1,16 +1,48 @@
 <template>
   <div id="homeLeftColumn">
     <div class="header">
+
+      <!--FILTER-->
+
       <v-popover>
         <img src="../assets/filter.svg" class="tooltip-target header-icon">
         <template slot="popover" class="tooltip-content">
-          <p>shirrin</p>
+          <div class="filter-container">
+            <label class="filter-label">Filter Projects</label>
+
+            <!--CHECKBOX-->
+
+            <div class="checkbox-container" v-for="(item, index) in filterType" :key="index">
+              <input type="checkbox" :id="`checkbox${index}`" name="filter" :value="item" class="checkbox"/>
+              <label :for="`checkbox${index}`" class="check"></label>
+              <label :for="`checkbox${index}`">{{ item }}</label>
+            </div>
+          </div>
         </template>
       </v-popover>
+
+      <!--SORT-->
+
       <v-popover>
         <img src="../assets/sort.svg" class="tooltip-target header-icon">
+        <template slot="popover" class="tooltip-content">
+          <div class="sort-container">
+            <label class="sort-label">sort Projects</label>
+
+          <!--RADIO BUTTON-->
+
+            <div class="radio-container" v-for="(item, index) in sortType" :key="index">
+              <input type="radio" :id="`radio${index}`" name="filter" :value="item" class="radio"/>
+              <label :for="`radio${index}`" class="check"></label>
+              <label :for="`radio${index}`">{{ item }}</label>
+            </div>
+          </div>
+        </template>
       </v-popover>
     </div>
+
+    <!--PROJECTS LIST-->
+
     <div class="entities">
       <div
         class="entity-details"
@@ -60,11 +92,12 @@
 
 <script>
 import { mapMutations, mapState } from 'vuex'
-
 export default {
   name: 'HomeLeftColumn',
   data () {
     return {
+      filterType: ['Global (Public)', 'Group 1', 'Group 2'],
+      sortType: ['Title', 'Last activity'],
       // TODO: Change below data to dynamic.
       unreadMessage: '299',
       eventLogMessage: '52'
