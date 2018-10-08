ff86a53809a5df441a740af3b4f07239cde3b903
mrastiak <armin.ayari@gmail.com>
2018-10-08 14:43:46 +0330

    Updated baseUrls, Closes #73

--- src/settings.js
+++ src/settings.js
@@ -2,10 +2,12 @@ export const DOLPHIN_BASE_URL = process.env.NODE_ENV === 'production'
   ? `${window.location.protocol}//${window.location.hostname}` : 'http://192.168.1.85'
 
 export const JAGUAR_BASE_URL = process.env.NODE_ENV === 'production'
-  ? `${window.location.protocol}//${window.location.hostname}:8082` : 'http://192.168.1.85:8082'
+  ? `${window.location.protocol}//${window.location.hostname.split('.')[0]}.cucumber.carrene.com`
+  : 'http://192.168.1.85:8082'
 
 export const CAS_BASE_URL = process.env.NODE_ENV === 'production'
-  ? `http://192.168.1.85:8081` : 'http://192.168.1.85:8081'
+  ? `${window.location.protocol}//${window.location.hostname.split('.')[0]}.cas.carrene.com`
+  : 'http://192.168.1.85:8081'
 
 export const SCOPES = ['title', 'email']
 
