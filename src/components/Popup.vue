54c11e07cc55729a2a0e1d88d6806023a2d8fa55
shirin <eghrarish@gmail.com>
2018-10-25 12:05:38 +0330

    Create popup component and style files, #110

---
+++ src/components/Popup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="popup">
+      <div class="popupBox">
+        <p>{{ message }}</p>
+        <div class="buttonContainer">
+          <button type="button" class="light-primary-button small" @click="$emit('confirm')">Yes</button>
+          <button type="button" class="primary-button small" @click="$emit('cancel')">No</button>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'popup',
+  props: ['message'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
