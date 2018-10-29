643501060b6e604039df143901dc9a06bada4b69
shirin <eghrarish@gmail.com>
2018-10-29 12:01:48 +0330

    Created loading component, #121

---
+++ src/components/Loading.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="Loading">
+    <div class="spinner">
+      <div class="right-side">
+        <div class="bar"></div>
+      </div>
+      <div class="left-side">
+        <div class="bar"></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Loading'
+}
+</script>
