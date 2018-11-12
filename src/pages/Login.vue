f8c4fbb5282edfe9f9ffce2bb8298b96d4967c4c
shirin <eghrarish@gmail.com>
2018-11-12 12:57:12 +0330

    Updated login page, closes #152

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -5,10 +5,11 @@
         <img src="../assets/maestro-light.svg">
         <h1>MAESTRO</h1>
       </div>
-      <p class="tabs">Home</p>
+      <p class="tabs">Why Maestro?</p>
       <p class="tabs">Services</p>
       <p class="tabs">Support</p>
       <p class="tabs">Help</p>
+      <button type="button" class="light-primary-button small" @click="login">Log in</button>
     </div>
     <div class="body">
       <div class="button-container">
@@ -17,7 +18,7 @@
           <p class="welcome">Welcome to <span>MAESTRO</span></p>
           <p class="message">LET'S EXPLORE THE WORLD</p>
         </div>
-        <button type="button" @click="login">Sign in</button>
+        <button type="button" @click="signup">Get started</button>
       </div>
       <img src="../assets/login-picture.svg">
     </div>
@@ -37,13 +38,22 @@ export default {
     }
   },
   methods: {
-    login () {
+    redirect (start) {
       let redirect = new URL(window.location.href).searchParams.get('redirectUri') || window.location.origin
       let url = new URL(`${CAS_FRONTEND_BASE_URL}/permissions`)
       url.searchParams.set('applicationId', APPLICATION_ID)
       url.searchParams.set('scopes', SCOPES.join(','))
       url.searchParams.set('redirectUri', encodeURI(redirect))
+      if (start) {
+        url.searchParams.set('start', true)
+      }
       window.location.assign(url.href)
+    },
+    signup () {
+      this.redirect(true)
+    },
+    login () {
+      this.redirect(false)
     }
   }
 }
