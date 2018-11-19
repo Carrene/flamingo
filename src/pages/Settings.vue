58ea6669d5240c4ddf819b647734909bbd15ed25
forough-jalili <foroughjalili@gmail.com>
2018-11-19 16:54:56 +0330

    Added notification icon and avatar to header, #158

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -5,6 +5,8 @@
 
     <div class="header">
       <p class="header-title">Personal Setting</p>
+      <img class="icon" src="../assets/notification.svg">
+      <img class="avatar" :src="picSrc">
     </div>
 
     <!-- CONTENT -->
@@ -21,17 +23,24 @@ import Profile from '../components/Profile'
 import SettingsRightColumn from '../components/SettingsRightColumn'
 import SettingsLeftColumn from '../components/SettingsLeftColumn'
 import SettingsMiddleColumn from '../components/SettingsMiddleColumn'
-import { mapState } from 'vuex'
+import server from '../server'
 
 export default {
   name: 'Settings',
   data () {
     return {
+      auth: server.authenticator
     }
   },
-  computed: mapState([
-    'selectedSettingsTab'
-  ]),
+  computed: {
+    picSrc () {
+      if (this.auth.member.avatar) {
+        return this.auth.member.avatar
+      } else {
+        return require('./../assets/profile-default-picture.svg')
+      }
+    }
+  },
   components: {
     Profile,
     SettingsLeftColumn,
