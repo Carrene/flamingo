a0a322c3ceb8ad4b17350496146d15999a996e1a
mrastiak <armin.ayari@gmail.com>
2018-12-01 20:15:13 +0330

    Removed v-tooltip from packages

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuelidate from 'vuelidate'
 import store from './store'
-import VTooltip from 'v-tooltip'
 import VueSimpleSVG from 'vue-simple-svg'
 
 Vue.config.productionTip = false
@@ -12,7 +10,6 @@ Vue.config.productionTip = false
 require('./styles/index.sass')
 require('./helpers')
 Vue.use(Vuelidate)
-Vue.use(VTooltip)
 Vue.use(VueSimpleSVG)
 
 /* eslint-disable no-new */
@@ -20,6 +17,8 @@ new Vue({
   el: '#app',
   router,
   store,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
