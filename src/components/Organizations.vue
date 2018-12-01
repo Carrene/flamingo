8fd4c78c78e6c8509447cf78027d083123629e4e
forough-jalili <foroughjalili@gmail.com>
2018-12-01 14:45:51 +0330

    Added style files and fixed layout, #180

---
+++ src/components/Organizations.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="organization">
+    <div class="org-action">
+      <p>Organization</p>
+      <button
+        class="primary-button small"
+        type="button"
+        disabled
+      >New</button>
+    </div>
+    <div class="org-info">
+      <img
+        src=""
+        alt=""
+      >
+      <button
+        class="light-primary-button small"
+        type="button"
+        disabled
+      >Leave</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Organizations',
+  data () {
+    return {
+    }
+  }
+}
+</script>
