b1d09eb65e7a7ff9de461061f568ae9b0ffa442f
mrastiak <armin.ayari@gmail.com>
2018-12-02 15:53:50 +0330

    Updated validation message components

--- src/components/ValidationMessage.vue
+++ src/components/ValidationMessage.vue
@@ -3,13 +3,13 @@
     class="validation-message error"
     v-if="validation.$error"
   >
+    <!-- TODO: Convert to one p element -->
     <p v-if="validation.required === false">{{ metadata.label }} is required</p>
     <p v-else-if="validation.minLength  === false">{{ metadata.label }} should be more than {{ metadata.minLength }} characters</p>
     <p v-else-if="validation.maxLength  === false">{{ metadata.label }} should be less than {{ metadata.maxLength }} characters</p>
     <p v-else-if="validation.minValue  === false">{{ metadata.label }} can't be less than {{ metadata.minimum }}</p>
     <p v-else-if="validation.maxValue  === false">{{ metadata.label }} can't be more than {{ metadata.maximum }}</p>
-    <!-- FIXME: change `pattern_description to patternDescription` -->
-    <p v-else-if="validation.pattern  === false">{{ metadata.pattern_description }}</p>
+    <p v-else-if="validation.pattern  === false">{{ metadata.patternDescription }}</p>
   </div>
   <div
     class="helper-text"
