4590300ce2399dcf2d21e2b344435fad79d0ed24
shirin <eghrarish@gmail.com>
2018-12-04 10:48:23 +0330

    Updated file preview, #170

--- src/components/FilePreview.vue
+++ src/components/FilePreview.vue
@@ -11,28 +11,24 @@
         />
       </div>
       <div class="file">
-        <span class="file-name">desktop-illus.png</span>
+        <span class="file-name" v-if="fileName">desktop-illus.png</span>
         <div class="image-box">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Felis_catus-cat_on_snow.jpg">
+          <img :src="previewImage">
         </div>
       </div>
-      <div class="file-details">
+      <div class="file-details" v-if="previewMessage">
         <h3 class="sender">
-          Lorem ipsum
+          {{ previewMessage.isMine ? 'Me' : previewMessage.title }}
         </h3>
         <p class="file-description">
-          Lorem ipsum dolor sit amet consectetur,
-          adipisicing elit. Soluta consectetur commodi saepe!
-          Facilis ducimus accusamus voluptatum eligendi quaerat,
-          maiores, temporibus omnis, voluptatibus ipsum nemo reiciendis rerum!
-          Veniam aliquam doloribus optio!
+          {{ previewMessage.body }}
         </p>
         <div class="file-metadata">
           <p class="file-date">
-            September 20
+            {{ formatDate(previewMessage.createdAt) }}
           </p>
           <p class="file-time">
-            05:30 PM
+            {{ formatTime(previewMessage.createdAt) }}
           </p>
         </div>
       </div>
@@ -43,8 +39,63 @@
 <script>
 export default {
   name: 'FilePreview',
+  props: ['previewImage', 'previewMessage'],
   data () {
     return {
+    }
+  },
+  methods: {
+    isThisYear (isoDateString) {
+      let now = new Date()
+      let date = new Date(isoDateString)
+      return now.getFullYear() === date.getFullYear()
+    },
+    formatDate (isoDateString) {
+      // TODO: Migrate to moment.js
+      isoDateString += 'Z'
+      let date = new Date(isoDateString)
+      let formatedDate = ''
+      // handling months and days
+      const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+      ]
+      formatedDate = `${months[date.getMonth()]} ${date.getDate()}`
+      // Handling year
+      if (!this.isThisYear(isoDateString)) {
+        formatedDate += `, ${date.getFullYear()}`
+      }
+      return formatedDate
+    },
+    formatTime (isoDateString) {
+      // TODO: Migrate to moment.js
+      isoDateString += 'Z'
+      let date = new Date(isoDateString)
+      let hour = date.getHours()
+      let amOrPm = 'AM'
+      if (hour >= 12) {
+        amOrPm = 'PM'
+      }
+      if (hour >= 13) {
+        hour -= 12
+      }
+      if (hour < 1) {
+        hour = 12
+      }
+      hour = hour.toLocaleString('en', { minimumIntegerDigits: 2 })
+      let minute = date.getMinutes().toLocaleString('en', { minimumIntegerDigits: 2 })
+      let formattedTime = `${hour}:${minute} ${amOrPm}`
+      return formattedTime
     }
   }
 }
