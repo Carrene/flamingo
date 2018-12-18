c6ca2cd6e28d2eebbdf95bcae5bedb366dc4fb49
mrastiak <armin.ayari@gmail.com>
2018-12-18 15:26:48 +0330

    Fixed a bug in login page

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -163,7 +163,7 @@ export default {
       showOrganizationList: false,
       organizations: [],
       selectedOrganization: null,
-      isClaimed: false
+      isClaimed: true
     }
   },
   validations () {
