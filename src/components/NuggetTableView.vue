ba7209a2c9a4255bb770bc313c38773e458ac504
mrastiak <armin.ayari@gmail.com>
2019-01-08 13:27:04 +0330

    Changed nuggetTableView to table element

--- src/components/NuggetTableView.vue
+++ src/components/NuggetTableView.vue
@@ -12,114 +12,119 @@
       v-else
     >
 
-      <!-- TABLE -->
-
-      <div class="table">
-        <div class="row header">
-          <div></div>
-          <div
-            v-for="header in headers"
-            :key="header.label"
-            class="cell"
-            :class="{active: header.isActive}"
-            @click="sort(header)"
-          >
-            <p :title="header.label">{{ header.label }}</p>
-            <simple-svg
-              :filepath="sortIconSrc"
-              :fill="sortIconColor"
-              class="icon"
-              v-if="header.isActive"
-              :class="{ascending: !nuggetSortCriteria.descending}"
-            ></simple-svg>
-          </div>
-        </div>
-        <div
-          :class="{selected: selectedNugget && selectedNugget.id === nugget.id}"
-          class="row content"
-          v-for="nugget in nuggetsOfSelectedProject"
-          :key="nugget.id"
-          @click.capture="selectNugget(nugget)"
-        >
-          <!-- TODO: add notifications later -->
-          <div class="notification cell">
-            <img
-              src="../assets/notification-dark.svg"
-              alt="notifications"
+      <table class="table">
+        <thead class="header">
+          <tr class="row">
+            <th class="cell notification"></th>
+            <th
+              v-for="header in headers"
+              :key="header.label"
+              class="cell"
+              :class="[{active: header.isActive}, header.className]"
+              @click="sort(header)"
             >
-          </div>
-          <div class="checkbox-container subscribe cell">
-            <input
-              type="checkbox"
-              :id="`checkbox${nugget.id}`"
-              name="subscribe"
-              class="checkbox"
-              :checked="nugget.isSubscribed"
-              @change="toggleSubscription(nugget)"
-            />
-            <label
-              :for="`checkbox${nugget.id}`"
-              class="check"
-            ></label>
-          </div>
-          <div
-            class="cell title"
-            :title="nugget.title"
+              <div class="title-container">
+                <p :title="header.label">{{ header.label }}</p>
+                <simple-svg
+                  :filepath="sortIconSrc"
+                  :fill="sortIconColor"
+                  class="icon"
+                  v-if="header.isActive"
+                  :class="{ascending: !nuggetSortCriteria.descending}"
+                ></simple-svg>
+              </div>
+            </th>
+          </tr>
+        </thead>
+        <tbody class="content">
+          <tr
+            :class="{selected: selectedNugget && selectedNugget.id === nugget.id}"
+            class="row"
+            v-for="nugget in nuggetsOfSelectedProject"
+            :key="nugget.id"
+            @click.capture="selectNugget(nugget)"
           >
-            <p>{{ nugget.title }}</p>
-          </div>
-          <div
-            :class="['pace', nugget.boarding]"
-            class="cell"
-            :title="nugget.boarding.formatText()"
-          >
-            <p>{{ nugget.boarding.formatText() }}</p>
-          </div>
-          <div
-            class="status cell"
-            :title="nugget.status.formatText()"
-          >
-            <p>{{ nugget.status.formatText() }}</p>
-          </div>
-          <div
-            class="priority cell"
-            :title="nugget.priority.formatText()"
-          >
-            <p>{{ nugget.priority.formatText() }}</p>
-          </div>
-          <div
-            class="kind cell"
-            :title="nugget.kind.formatText()"
-          >
-            <p>{{ nugget.kind.formatText() }}</p>
-          </div>
-          <div
-            class="phase cell"
-            :title="getPhaseTitles(nugget.phases).join(', ')"
-            @click="showAssignModal"
-          >
-            <p>{{ getPhaseTitles(nugget.phases).join(', ') || 'Triage' }}</p>
-          </div>
-          <div
-            class="days cell"
-            :title="nugget.days"
-          >
-            <p>{{ nugget.days }}</p>
-          </div>
-          <div
-            class="target-date cell"
-            :title="formatTargetDate(nugget.dueDate)"
-          >
-            <p>{{ formatTargetDate(nugget.dueDate) }}</p>
-          </div>
-          <div
-            class="created-at cell"
-            :title="formatTargetDate(nugget.createdAt)"
-          >
-            <p>{{ formatTargetDate(nugget.createdAt) }}</p>
-          </div>
-        </div>
-      </div>
+            <td class="notification cell">
+              <img
+                src="../assets/notification-dark.svg"
+                alt="notifications"
+              >
+            </td>
+            <td class="subscribe cell">
+              <div class="checkbox-container">
+                <input
+                  type="checkbox"
+                  :id="`checkbox${nugget.id}`"
+                  name="subscribe"
+                  class="checkbox"
+                  :checked="nugget.isSubscribed"
+                  @change="toggleSubscription(nugget)"
+                />
+                <label
+                  :for="`checkbox${nugget.id}`"
+                  class="check"
+                ></label>
+              </div>
+            </td>
+            <td
+              class="cell title"
+              :title="nugget.title"
+            >
+              <p>{{ nugget.title }}</p>
+            </td>
+            <td
+              :class="['pace', nugget.boarding]"
+              class="cell"
+              :title="nugget.boarding.formatText()"
+            >
+              <p>{{ nugget.boarding.formatText() }}</p>
+            </td>
+            <td
+              class="status cell"
+              :title="nugget.status.formatText()"
+            >
+              <p>{{ nugget.status.formatText() }}</p>
+            </td>
+            <td
+              class="priority cell"
+              :title="nugget.priority.formatText()"
+            >
+              <p>{{ nugget.priority.formatText() }}</p>
+            </td>
+            <td
+              class="kind cell"
+              :title="nugget.kind.formatText()"
+            >
+              <p>{{ nugget.kind.formatText() }}</p>
+            </td>
+            <td
+              class="phase cell"
+              :title="getPhaseTitles(nugget.phases).join(', ')"
+              @click="showAssignModal"
+            >
+              <p>{{ getPhaseTitles(nugget.phases).join(', ') || 'Triage' }}</p>
+            </td>
+            <td
+              class="days cell"
+              :title="nugget.days"
+            >
+              <p>{{ nugget.days }}</p>
+            </td>
+            <td
+              class="target-date cell"
+              :title="formatTargetDate(nugget.dueDate)"
+            >
+              <p>{{ formatTargetDate(nugget.dueDate) }}</p>
+            </td>
+            <td
+              class="created-at cell"
+              :title="formatTargetDate(nugget.createdAt)"
+            >
+              <p>{{ formatTargetDate(nugget.createdAt) }}</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
     <assign
       ref="assign"
@@ -173,52 +178,62 @@ export default {
         {
           label: this.nuggetMetadata.fields.isSubscribed.label,
           isActive: this.nuggetSortCriteria.field === 'isSubscribed',
-          field: 'isSubscribed'
+          field: 'isSubscribed',
+          className: 'subscribe'
         },
         {
           label: this.nuggetMetadata.fields.title.label,
           isActive: this.nuggetSortCriteria.field === 'title',
-          field: 'title'
+          field: 'title',
+          className: 'title'
         },
         {
           label: this.nuggetMetadata.fields.boarding.label,
           isActive: this.nuggetSortCriteria.field === 'boarding',
-          field: 'boarding'
+          field: 'boarding',
+          className: 'pace'
         },
         {
           label: this.nuggetMetadata.fields.status.label,
           isActive: this.nuggetSortCriteria.field === 'status',
-          field: 'status'
+          field: 'status',
+          className: 'status'
         },
         {
           label: this.nuggetMetadata.fields.priority.label,
           isActive: this.nuggetSortCriteria.field === 'priority',
-          field: 'priority'
+          field: 'priority',
+          className: 'priority'
         },
         {
           label: this.nuggetMetadata.fields.kind.label,
           isActive: this.nuggetSortCriteria.field === 'kind',
-          field: 'kind'
+          field: 'kind',
+          className: 'kind'
         },
         {
           label: this.nuggetMetadata.fields.phaseId.label,
           isActive: this.nuggetSortCriteria.field === 'phaseId',
-          field: 'phaseId'
+          field: 'phaseId',
+          className: 'phase'
         },
         {
           label: this.nuggetMetadata.fields.days.label,
           isActive: this.nuggetSortCriteria.field === 'days',
-          field: 'days'
+          field: 'days',
+          className: 'days'
         },
         {
           label: this.nuggetMetadata.fields.dueDate.label,
           isActive: this.nuggetSortCriteria.field === 'dueDate',
-          field: 'dueDate'
+          field: 'dueDate',
+          className: 'target-date'
         },
         {
           label: this.nuggetMetadata.fields.createdAt.label,
           isActive: this.nuggetSortCriteria.field === 'createdAt',
-          field: 'createdAt'
+          field: 'createdAt',
+          className: 'created-at'
         }
       ]
     },
