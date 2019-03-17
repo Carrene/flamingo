7d7342b1f4dffbccd1a254fdc40df11b218d195a
forough-jalili <foroughjalili@gmail.com>
2019-03-17 14:36:58 +0330

    Refactored codes and removed useless files

--- src/components/SettingsNavigation.vue
+++ src/components/SettingsNavigation.vue
@@ -13,19 +13,6 @@
         <p class="title">{{ tab.title }}</p>
       </router-link>
     </ul>
-    <ul class="tabs">
-      <li class="header">Maestro Settings</li>
-      <router-link
-        v-for="(tab, routeName) in maestroSettingTabs"
-        tag="li"
-        :to="tab.path"
-        class="tab"
-        :active-class="tab.activeClass"
-        :key="routeName"
-      >
-        <p class="title">{{ tab.title }}</p>
-      </router-link>
-    </ul>
 
     <!-- FOOTER -->
 
@@ -63,57 +50,6 @@ export default {
             name: 'Organizations'
           },
           title: 'Organizations',
-          activeClass: 'selected'
-        }
-      },
-      maestroSettingTabs: {
-        users: {
-          path: {
-            name: 'Users'
-          },
-          title: 'Users',
-          activeClass: 'selected'
-        },
-        groups: {
-          path: {
-            name: 'Groups'
-          },
-          title: 'Groups',
-          activeClass: 'selected'
-        },
-        skills: {
-          path: {
-            name: 'Skills'
-          },
-          title: 'Skills',
-          activeClass: 'selected'
-        },
-        phases: {
-          path: {
-            name: 'Phases'
-          },
-          title: 'Phases',
-          activeClass: 'selected'
-        },
-        workflows: {
-          path: {
-            name: 'Workflows'
-          },
-          title: 'Workflows',
-          activeClass: 'selected'
-        },
-        tags: {
-          path: {
-            name: 'Tags'
-          },
-          title: 'Tags',
-          activeClass: 'selected'
-        },
-        calender: {
-          path: {
-            name: 'Calender'
-          },
-          title: 'Calender',
           activeClass: 'selected'
         }
       },
