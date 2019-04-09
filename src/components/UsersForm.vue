6c57faee337042007fa53ff58a4430ae9f71e8c5
shirin <eghrarish@gmail.com>
2019-04-09 14:04:28 +0430

    Added validation message component in user form, #291

--- src/components/UsersForm.vue
+++ src/components/UsersForm.vue
@@ -4,18 +4,22 @@
     <div class="header">
       <button
         type="button"
-        class="secondary-button small disabled"
+        class="secondary-button"
+        :disabled="$v.user.$invalid"
       >Save</button>
     </div>
 
     <div class="content">
-      <profile-picture class="profile-picture" :picture="user.avatar" />
+      <profile-picture
+        class="profile-picture"
+        :picture="user.avatar"
+      />
 
       <div class="input-container">
         <label
-          for="userName"
+          :for="organizationMemberMetadata.fields.title.label"
           class="label"
-        >{{ memberMetadata.fields.title.label }}</label>
+        >{{ organizationMemberMetadata.fields.title.label }}</label>
         <input
           type="text"
           class="light-primary-input disabled"
@@ -26,9 +30,9 @@
 
       <div class="input-container">
         <label
-          for="fullName"
+          :for="organizationMemberMetadata.fields.name.label"
           class="label"
-        >{{ memberMetadata.fields.name.label }}</label>
+        >{{ organizationMemberMetadata.fields.name.label }}</label>
         <input
           type="text"
           class="light-primary-input disabled"
@@ -38,9 +42,9 @@
       </div>
       <div class="input-container">
         <label
-          for="email"
+          :for="organizationMemberMetadata.fields.email.label"
           class="label"
-        >{{ memberMetadata.fields.email.label }}</label>
+        >{{ organizationMemberMetadata.fields.email.label }}</label>
         <input
           type="text"
           class="light-primary-input disabled"
@@ -53,23 +57,39 @@
 
       <!-- <div class="input-container">
         <label
-          for="skills"
+          :for="organizationMemberMetadata.fields.skillId.label"
           class="label"
-        >{{ memberMetadata.fields.skillId.label }}</label>
+          :class="{error: $v.user.skillId.$error}"
+        >{{ organizationMemberMetadata.fields.skillId.label }}</label>
         <input
           type="text"
           class="light-primary-input"
+          @input="$v.user.skillId.$touch"
+          @focus="$v.user.skillId.$reset"
+          :class="{error: $v.user.skillId.$error}"
         >
+        <validation-message
+          :validation="$v.skillId.title"
+          :metadata="organizationMemberMetadata.fields.title"
+        />
       </div> -->
       <!-- <div class="input-container">
         <label
-          for="group"
+          :for="organizationMemberMetadata.fields.group.label"
           class="label"
+          :class="{error: $v.user.group.$error}"
         >Group</label>
         <input
           type="text"
           class="light-primary-input"
+          @input="$v.user.group.$touch"
+          @focus="$v.user.group.$reset"
+          :class="{error: $v.user.group.$error}"
         >
+        <validation-message
+          :validation="$v.group.title"
+          :metadata="organizationMemberMetadata.fields.title"
+        />
       </div> -->
     </div>
 
@@ -78,6 +98,13 @@
 
 <script>
 import server from '../server'
+import { mapState } from 'vuex'
+const Loading = () => import(
+  /* webpackChunkName: "Loading" */ './Loading'
+)
+const ValidationMessage = () => import(
+  /* webpackChunkName: "ValidationMessage" */ './ValidationMessage'
+)
 
 const ProfilePicture = () => import(
   /* webpackChunkName: "ProfilePicture" */ '../components/ProfilePicture'
@@ -87,22 +114,42 @@ export default {
   data () {
     return {
       user: null,
-      memberMetadata: server.metadata.models.OrganizationMember
+      organizationMemberMetadata: server.metadata.models.OrganizationMember
     }
+  },
+  validations () {
+    return {
+      user: {
+        skillId: this.organizationMemberMetadata.fields.skillId.createValidator()
+        // group: this.organizationMemberMetadata.fields.group.createValidator()
+      }
+    }
+  },
+  computed: {
+    ...mapState([
+      'OrganizationMember'
+    ])
   },
   props: {
     selectedUser: Object
   },
-  watch: {
-    selectedUser: {
-      deep: true,
-      immediate: true,
-      handler (newValue) {
-        this.user = Object.assign({}, newValue)
-      }
+  methods: {
+    async getSelectedUser () {
+      this.loading = false
+      let response = await this.OrganizationMember.get(this.selectedUser.id).send()
+      this.user = response.models[0]
+      this.loading = false
     }
   },
+  beforeMount () {
+    this.user = new this.OrganizationMember()
+  },
+  mounted () {
+    this.getSelectedUser()
+  },
   components: {
+    Loading,
+    ValidationMessage,
     ProfilePicture
   }
 }
