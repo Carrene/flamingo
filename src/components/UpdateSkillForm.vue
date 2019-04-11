bc6ffb3a86ceb3692daf23590ae0620835911726
forough-jalili <foroughjalili@gmail.com>
2019-04-11 12:41:45 +0430

    Worked on update skill form functionality, #306

--- src/components/UpdateSkillForm.vue
+++ src/components/UpdateSkillForm.vue
@@ -10,6 +10,10 @@
       >New Skill</button>
     </div>
 
+    <!-- LOADING -->
+
+    <loading v-if="loading" />
+
     <!-- CONTENT -->
 
     <div class="content">
@@ -17,38 +21,74 @@
         <label
           for="skillName"
           class="label"
-        >Skill Name</label>
+        >{{ skillMetadata.fields.title.label }}</label>
         <input
           type="text"
           class="light-primary-input"
+          v-model.trim="skill.title"
         >
       </div>
       <div class="input-container">
         <label
           for="skillName"
           class="label"
-        >Skill Description</label>
+        >{{ skillMetadata.fields.description.label }}</label>
         <div class="textarea-container medium">
-          <textarea class="light-primary-input"></textarea>
+          <textarea
+            class="light-primary-input"
+            v-model.trim="skill.description"
+          ></textarea>
+          <p
+            class="character-count"
+            v-if="skill.description"
+          >
+            {{ skill.description.length }}/{{skillMetadata.fields.description.maxLength }}
+          </p>
         </div>
-        <!-- FIXME: NOT IMPLEMENTED YET -->
-        <!-- <p
-          class="character-count"
-          v-if="group.description"
-        >
-          {{ skill.description.length }}/{{skill.fields.description.maxLength }}
-        </p> -->
       </div>
     </div>
   </form>
 </template>
 
 <script>
+import server from '../server'
+import { mapState } from 'vuex'
 export default {
   name: 'UpdateSkillForm',
   data () {
     return {
+      skill: null,
+      loading: false,
+      skillMetadata: server.metadata.models.Skill
     }
+  },
+  props: {
+    selectedSkill: Object
+  },
+  computed: {
+    ...mapState([
+      'Skill',
+      'skills'
+    ])
+  },
+  watch: {
+    'selectedSkill.id' () {
+      this.getSelectedSkill()
+    }
+  },
+  methods: {
+    async getSelectedSkill () {
+      this.loading = false
+      let response = await this.Skill.get(this.selectedSkill.id).send()
+      this.skill = response.models[0]
+      this.loading = false
+    }
+  },
+  mounted () {
+    this.getSelectedSkill()
+  },
+  beforeMount () {
+    this.skill = new this.Skill()
   }
 }
 </script>
