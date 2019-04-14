ed709beb17ed27e38bdd79037fbd91a2e3f01d09
forough-jalili <foroughjalili@gmail.com>
2019-04-14 15:02:17 +0430

    Added v-select component for skill selection, #319

--- src/components/NewPhasePopup.vue
+++ src/components/NewPhasePopup.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="newPhasePopup">
+
+    <!-- HEADER -->
+
     <div class="popup-header">
       <p>Add new phase</p>
       <simple-svg
@@ -9,32 +12,47 @@
         @click.native="$emit('close')"
       />
     </div>
+
+    <!-- FORM -->
+
     <form class="popup-content">
+
+      <!-- NAME INPUT -->
+
       <div class="input-container">
         <label
           for="phaseName"
           class="label"
-        >Phase Name</label>
+        >{{ phaseMetadata.fields.title.label }}</label>
         <input
           type="text"
           class="light-primary-input"
+          v-model="phase.title"
         >
       </div>
+
+      <!-- SKILL INPUT -->
+
+      <div class="input-container">
+        <label
+          :for="phaseMetadata.fields.skillId.label"
+          class="label"
+        >{{ phaseMetadata.fields.skillId.label }}</label>
+        <v-select
+          :options="skills"
+          label="name"
+          index="id"
+          v-model="phase.skillId"
+        ></v-select>
+      </div>
+
+      <!-- DESCRIPTION -->
+
       <div class="input-container">
         <label
           for="phaseName"
           class="label"
-        >Associated Skills</label>
-        <input
-          type="text"
-          class="light-primary-input"
-        >
-      </div>
-      <div class="input-container">
-        <label
-          for="phaseName"
-          class="label"
-        >Phase Description</label>
+        >Description</label>
         <div class="textarea-container medium">
           <textarea class="light-primary-input"></textarea>
         </div>
@@ -46,6 +64,9 @@
             {{ phase.description.length }}/{{phase.fields.description.maxLength }}
           </p> -->
       </div>
+
+      <!-- ACTION -->
+
       <button
         type="button"
         class="secondary-button"
@@ -55,11 +76,30 @@
 </template>
 
 <script>
+import server from '../server'
+import { mapState, mapActions } from 'vuex'
 export default {
   name: 'newPhasePopup',
   data () {
     return {
+      phaseMetadata: server.metadata.models.Phase,
+      phase: null
     }
+  },
+  computed: {
+    ...mapState([
+      'Phase',
+      'skills'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'listSkills'
+    ])
+  },
+  beforeMount () {
+    this.listSkills()
+    this.phase = new this.Phase()
   }
 }
 </script>
