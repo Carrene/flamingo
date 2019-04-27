b39e46305e7f7a49c1068208a2d0c767f5d889e4
forough-jalili <foroughjalili@gmail.com>
2019-04-27 15:58:02 +0430

    Updated name of Account page and organizations files, #357

--- src/pages/Security.vue
+++ src/pages/Security.vue
@@ -7,7 +7,7 @@
 
       <!-- HEADER TITLE -->
 
-      <p class="title">Account</p>
+      <p class="title">Security</p>
 
       <!-- ACTION -->
 
@@ -117,7 +117,7 @@ const Snackbar = () => import(
 )
 export default {
   mixins: [clickout],
-  name: 'Account',
+  name: 'Security',
   data () {
     return {
       accountCredentials: {
