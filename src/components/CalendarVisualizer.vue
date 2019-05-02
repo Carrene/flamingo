6ddb89c58cda569f2a49b1e076009cf44fb5d10b
Mahdi Aryayi <mahdiaryayi@gmail.com>
2019-05-02 10:44:51 +0430

    Feature/calendar (#377)
    * Worked on calendar page and forms layout, #329
    
    * Finished calendar page layout, closes #329
    
    * Worked on calendar visualizer component layout, #329
    
    * Finished calendar visualizer layout, closes #329
    
    * Worked on calendar visualizer component functionality
    
    * Finished calendar visualizer component
    
    * Created event and event types class
    
    * Worked on event list functionality, closes #359
    
    * Worked on create event functionality, #358
    
    * Finished create event functionality, closes #358
    
    * Updated date column in event list, #359
    
    * Worked on selected event functionality, #359
    
    * Finished update event form functionality, closes #360
    
    * Added title column and removed description column from event table, #359
    
    * Updated event types column in event table,closes #359
    
    * Added title field and removed description field from new event form, closes #358
    
    * Added repeat field in event tabel and forms
    
    * Updated calendar visualizer component

---
+++ src/components/CalendarVisualizer.vue
@@ -0,0 +1,191 @@
+<template>
+  <div id="calendarVisualizer">
+    <div class="header">
+      <p class="title">Calendar</p>
+    </div>
+    <div class="content">
+      <div class="calendar-box-header">
+        <simple-svg
+          :filepath="require('@/assets/calendar.svg')"
+          fill="#232323"
+          class="calendar-icon"
+        />
+        <button
+          type="button"
+          class="secondary-button outlined"
+          @click="goToday"
+        >Today</button>
+      </div>
+      <div class="calendar-box-content">
+
+        <div class="calendar-container">
+          <sweet-calendar
+            :eventCategories="categoryActivated"
+            :events="decoratedEvents"
+            :offDays="offDays"
+            ref="calendar"
+          />
+        </div>
+
+        <div class="week-days">
+          <div class="week-days-header">Weekly Off-Days</div>
+          <div class="week-days-content">
+            <div class="days">
+              <loading-checkbox
+                v-for="day of daysOfWeek"
+                :key="day.id"
+                class="check-box"
+                :checked="day.selected === true"
+                @click.native="day.selected = !day.selected"
+                :size="14"
+                :fontSize="14"
+                :loading="false"
+                :gap="8"
+                :label="day.name"
+                borderRadius="3px"
+                checkedBorderColor="#008290"
+                checkedBackgroundColor="#008290"
+                spinnerColor="#008290"
+              ></loading-checkbox>
+            </div>
+          </div>
+        </div>
+
+        <div class="category">
+          <div class="category-header">
+            Category
+          </div>
+          <div class="category-content">
+            <loading-checkbox
+              v-for="eventCategory of eventCategories"
+              :key="eventCategory.id"
+              class="check-box"
+              :checked="eventCategory.selected === true"
+              @click.native="eventCategory.selected = !eventCategory.selected"
+              :size="14"
+              :fontSize="14"
+              :loading="false"
+              :gap="8"
+              :label="eventCategory.name"
+              borderRadius="3px"
+              checkedBorderColor="#008290"
+              checkedBackgroundColor="#0B2B53"
+              spinnerColor="#008290"
+            ></loading-checkbox>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import LoadingCheckbox from 'vue-loading-checkbox'
+import 'vue-loading-checkbox/dist/LoadingCheckbox.css'
+import SweetCalendar from 'vue-sweet-calendar'
+import 'vue-sweet-calendar/dist/SweetCalendar.css'
+import moment from 'moment'
+
+export default {
+  name: 'CalendarVisualizer',
+  data () {
+    return {
+      eventCategories: [
+        {
+          id: 1,
+          name: 'Personal',
+          textColor: 'white',
+          backgroundColor: '#194173',
+          selected: true
+        },
+        {
+          id: 2,
+          name: 'Company-wide',
+          textColor: 'white',
+          backgroundColor: '#0B2B53',
+          selected: true
+        }
+      ],
+      daysOfWeek: [
+        {
+          name: 'Sunday',
+          id: 1,
+          selected: true
+        },
+        {
+          name: 'Monday',
+          id: 2,
+          selected: false
+        },
+        {
+          name: 'Tuesday',
+          id: 3,
+          selected: false
+        },
+        {
+          name: 'Wednesday',
+          id: 4,
+          selected: false
+        },
+        {
+          name: 'Thursday',
+          id: 5,
+          selected: false
+        },
+        {
+          name: 'Friday',
+          id: 6,
+          selected: false
+        },
+        {
+          name: 'Saturday',
+          id: 7,
+          selected: true
+        }
+      ]
+    }
+  },
+  computed: {
+    decoratedEvents () {
+      return this.events.map(event => {
+        return {
+          categoryId: event.eventTypeId,
+          start: moment(event.startDate).format('YYYY-MM-DD'),
+          end: moment(event.endDate).format('YYYY-MM-DD'),
+          repeat: event.repeat,
+          title: event.title
+        }
+      })
+    },
+    offDays () {
+      return this.daysOfWeek.reduce((acc, day) => {
+        if (day.selected) {
+          acc.push(day.id)
+        }
+        return acc
+      }, [])
+    },
+    categoryActivated () {
+      return this.eventCategories.filter(category => category.selected === true)
+    },
+    ...mapState([
+      'events'
+    ])
+  },
+  methods: {
+    goToday () {
+      this.$refs.calendar.goToday()
+    },
+    toggleOffDay (dayId) { },
+    addToOfDays (dayId) {
+      this.offDays.push(dayId)
+    }
+  },
+  components: {
+    LoadingCheckbox,
+    SweetCalendar
+  }
+}
+</script>
---
+++ src/components/UpdateEventForm.vue
@@ -0,0 +1,334 @@
+<template>
+  <form
+    id="updateEventForm"
+    @submit.prevent="update"
+  >
+
+    <!-- HEADER -->
+
+    <div class="header">
+      <button
+        type="button"
+        class="secondary-button"
+        @click="$emit('showNewEventForm')"
+        v-if="event.__status__ !== 'dirty'"
+      >New Event</button>
+      <button
+        type="submit"
+        class="secondary-button"
+        v-else
+      >Save</button>
+    </div>
+    <loading v-if="loading" />
+
+    <!-- CONTENT -->
+
+    <div
+      class="content"
+      v-if="!loading"
+    >
+      <!-- EVENT NAME -->
+
+      <div class="input-container">
+        <label
+          for="eventMetadata.fields.title.label"
+          class="label"
+          :class="{error: $v.event.title.$error}"
+        >{{ eventMetadata.fields.title.label }}</label>
+        <input
+          type="text"
+          class="light-primary-input"
+          :class="{error: $v.event.title.$error}"
+          v-model.trim="event.title"
+          @input="$v.event.title.$touch"
+          @focus="$v.event.title.$reset"
+        >
+        <validation-message
+          :validation="$v.event.title"
+          :metadata="eventMetadata.fields.title"
+        />
+      </div>
+      <!-- EVENT TYPE -->
+
+      <div class="input-container">
+        <label
+          for="eventMetadata.fields.eventTypeId.label"
+          class="label"
+        >{{ eventMetadata.fields.eventTypeId.label }}</label>
+        <v-select
+          :options="eventTypes"
+          index="id"
+          label="title"
+          v-model.trim="event.eventTypeId"
+          :clearable="!$v.event.eventTypeId.required"
+        ></v-select>
+        <validation-message
+          :validation="$v.event.eventTypeId"
+          :metadata="eventMetadata.fields.eventTypeId"
+        />
+      </div>
+
+      <!-- START DATE -->
+
+      <div class="input-container">
+        <label
+          for="eventMetadata.fields.startDate.label"
+          class="label"
+        >{{ eventMetadata.fields.startDate.label }}</label>
+        <div class="datepicker-container">
+          <input
+            type="text"
+            :value="startDate"
+            class="light-primary-input calendar"
+            @click="toggleStartDateDatepicker"
+            ref="startDate"
+            @change="$v.event.startDate.$touch"
+            @keyup.enter="toggleStartDateDatepicker"
+            readonly
+          >
+          <div
+            v-if="showStartDateDatepicker"
+            class="datepicker"
+            v-on-clickout="toggleStartDateDatepicker.bind(undefined, false)"
+          >
+            <custom-datepicker
+              primary-color="#2F2445"
+              :wrapperStyles="datepickerOptions.wrapperStyles"
+              @dateSelected="setStartDate($event)"
+              :date="event.startDate"
+              :limits="datepickerOptions.limits"
+            />
+          </div>
+        </div>
+        <validation-message
+          :validation="$v.event.startDate"
+          :metadata="eventMetadata.fields.startDate"
+        />
+      </div>
+
+      <!-- END DATE -->
+
+      <div class="input-container">
+        <label
+          for="eventMetadata.fields.endDate.label"
+          class="label"
+        >{{ eventMetadata.fields.endDate.label }}</label>
+        <div class="datepicker-container">
+          <input
+            type="text"
+            :value="endDate"
+            class="light-primary-input calendar"
+            @click="toggleEndDateDatepicker"
+            ref="endDate"
+            @change="$v.event.endDate.$touch"
+            @keyup.enter="toggleEndDateDatepicker"
+            readonly
+          >
+          <div
+            v-if="showEndDateDatepicker"
+            class="datepicker"
+            v-on-clickout="toggleEndDateDatepicker.bind(undefined, false)"
+          >
+            <custom-datepicker
+              primary-color="#2F2445"
+              :wrapperStyles="datepickerOptions.wrapperStyles"
+              @dateSelected="setEndDate($event)"
+              :date="event.endDate"
+              :limits="datepickerOptions.limits"
+            />
+          </div>
+        </div>
+        <validation-message
+          :validation="$v.event.endDate"
+          :metadata="eventMetadata.fields.endDate"
+        />
+      </div>
+
+      <!-- REPEATE -->
+
+      <div class="input-container">
+        <label
+          for="eventMetadata.fields.repeat.label"
+          class="label"
+        >{{ eventMetadata.fields.repeat.label }}</label>
+        <v-select
+          :options="repeatItems"
+          index="value"
+          v-model.trim="event.repeat"
+          :clearable="!$v.event.repeat.required"
+        ></v-select>
+        <validation-message
+          :validation="$v.event.repeat"
+          :metadata="eventMetadata.fields.repeat"
+        />
+      </div>
+      <snackbar
+        :status="status"
+        :message="message"
+        @close="clearMessage"
+      ></snackbar>
+    </div>
+  </form>
+</template>
+
+<script>
+import server from '../server'
+import { mapState } from 'vuex'
+import moment from 'moment'
+import CustomDatepicker from 'vue-custom-datepicker'
+import { mixin as clickout } from 'vue-clickout'
+import { updateModel } from './../helpers.js'
+
+const Snackbar = () => import(
+  /* webpackChunkName: "Snackbar" */ './Snackbar'
+)
+const Loading = () => import(
+  /* webpackChunkName: "Loading" */ './Loading'
+)
+const ValidationMessage = () => import(
+  /* webpackChunkName: "ValidationMessage" */ './ValidationMessage'
+)
+
+export default {
+  mixins: [clickout],
+  name: 'UpdateEventForm',
+  data () {
+    return {
+      loading: false,
+      status: null,
+      message: null,
+      event: null,
+      eventMetadata: server.metadata.models.Event,
+      showStartDateDatepicker: false,
+      showEndDateDatepicker: false,
+      repeatItems: [{ label: 'Never', value: 'never' }, { label: 'Monthly', value: 'monthly' }, { label: 'Yearly', value: 'yearly' }],
+      datepickerOptions: {
+        wrapperStyles: {
+          width: '100%',
+          background: '#194173',
+          color: '#ffffff',
+          position: 'relative'
+        },
+        limits: {
+          start: moment().format('YYYY-MM-DD'),
+          end: null
+        }
+      }
+    }
+  },
+  props: {
+    selectedEvent: Object
+  },
+  validations () {
+    return {
+      event: {
+        title: this.eventMetadata.fields.title.createValidator(),
+        eventTypeId: this.eventMetadata.fields.eventTypeId.createValidator(),
+        startDate: this.eventMetadata.fields.startDate.createValidator(),
+        endDate: this.eventMetadata.fields.endDate.createValidator(),
+        repeat: this.eventMetadata.fields.repeat.createValidator()
+      }
+    }
+  },
+  computed: {
+    startDate () {
+      if (this.event.startDate) {
+        return moment(this.event.startDate).format('YYYY-MM-DD')
+      } else {
+        return null
+      }
+    },
+    endDate () {
+      if (this.event.endDate) {
+        return moment(this.event.endDate).format('YYYY-MM-DD')
+      } else {
+        return null
+      }
+    },
+    ...mapState([
+      'Event',
+      'eventTypes',
+      'events'
+    ])
+  },
+  watch: {
+    'selectedEvent.id': {
+      immediate: true,
+      handler (newValue) {
+        this.getSelectedEvent(newValue)
+      }
+    }
+  },
+  methods: {
+    update () {
+      this.event.save().send().then(async resp => {
+        this.status = resp.status
+        this.message = 'Event was updated.'
+        await updateModel(this.events, this.event)
+        setTimeout(() => {
+          this.clearMessage()
+        }, 3000)
+      }).catch(resp => {
+        this.status = resp.status
+        this.message = resp.error
+        setTimeout(() => {
+          this.clearMessage()
+        }, 3000)
+      }).finally(() => {
+        this.loading = false
+      })
+    },
+    async getSelectedEvent (eventId) {
+      this.loading = false
+      let response = await this.Event.get(eventId).send()
+      this.event = response.models[0]
+      this.loading = false
+    },
+    clearMessage () {
+      this.status = null
+      this.message = null
+    },
+    setStartDate (date) {
+      // Checking if the date has been changed
+      this.event.startDate = moment(date).format('YYYY-MM-DD')
+      this.showStartDateDatepicker = false
+      this.$refs.startDate.focus()
+      if (this.event.startDate !== moment(date).format('YYYY-MM-DD')) {
+        this.$v.event.startDate.$touch()
+      }
+    },
+    toggleStartDateDatepicker (value) {
+      if (typeof value === 'boolean') {
+        this.showStartDateDatepicker = value
+      } else {
+        this.showStartDateDatepicker = !this.showStartDateDatepicker
+      }
+    },
+    setEndDate (date) {
+      // Checking if the date has been changed
+      this.event.endDate = moment(date).format('YYYY-MM-DD')
+      this.showEndDateDatepicker = false
+      this.$refs.endDate.focus()
+      if (this.event.endDate !== moment(date).format('YYYY-MM-DD')) {
+        this.$v.event.endDate.$touch()
+      }
+    },
+    toggleEndDateDatepicker (value) {
+      if (typeof value === 'boolean') {
+        this.showEndDateDatepicker = value
+      } else {
+        this.showEndDateDatepicker = !this.showEndDateDatepicker
+      }
+    }
+  },
+  components: {
+    Loading,
+    ValidationMessage,
+    Snackbar,
+    CustomDatepicker
+  },
+  beforeMount () {
+    this.event = new this.Event()
+  }
+}
+</script>
