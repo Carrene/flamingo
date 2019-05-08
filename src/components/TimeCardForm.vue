18b50e3a7b51943977fd9f5a0e15cf76b49450ce
forough-jalili <foroughjalili@gmail.com>
2019-05-08 10:37:44 +0430

    Added a component for time card form, #391

---
+++ src/components/TimeCardForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="timeCardForm">
+
+    <!-- HEADER -->
+
+    <div class="header">
+      <button
+        type="button"
+        class="secondary-button outlined small disabled"
+      >Submit Time Card</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TimeCardForm',
+  data () {
+    return {}
+  }
+}
+</script>
