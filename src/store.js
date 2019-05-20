df19380c1c84f00664453d1d6be6e60aa0b46076
eghrarish <36629847+eghrarish@users.noreply.github.com>
2019-05-20 13:23:40 +0430

    Fix some bug in assigned page (#419)
    * Updated response time and time card in columns in tables, closes #417
    
    * Added itemstotal count in sidebar, closes #418
    
    * Removed useless code in store file

--- src/store.js
+++ src/store.js
@@ -335,6 +335,14 @@ export default new Vuex.Store({
           title: 'Triage'
         }
       ].concat(state.phasesOfSelectedWorkflow)
+    },
+    totalItemCount (state) {
+      return (
+        state.inprocessCounter +
+        state.upcomingCounter +
+        state.needEstimateCounter +
+        state.newlyAssignedCounter
+      )
     }
   },
   actions: {
@@ -1514,8 +1522,28 @@ export default new Vuex.Store({
 
       store.commit('setUpcomingItems', resps[3].models)
       store.commit('setUpcomingItemsCounter', resps[3].totalCount)
+    },
 
-      // store.commit('selectItem', response.models[0])
+    async listItemsCount (store) {
+      const filters = [
+        'newlyAssigned',
+        'needEstimate',
+        'inProcessNuggets',
+        'upcomingNuggets'
+      ]
+      let requests = []
+      for (let filter of filters) {
+        requests.push(store.state.Item.load({ zone: filter }).send())
+      }
+      let resps = await Promise.all(requests)
+
+      store.commit('setNewlyAssignedCounter', resps[0].totalCount)
+
+      store.commit('setNeedEstimateCounter', resps[1].totalCount)
+
+      store.commit('setInprocessCounter', resps[2].totalCount)
+
+      store.commit('setUpcomingItemsCounter', resps[3].totalCount)
     },
 
     // DAILY REPORT ACTIONS
