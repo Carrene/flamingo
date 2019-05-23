51181104737375f7aa6b88d2d973cba2e0f7f287
ghazal-khaki <47708505+ghazal-khaki@users.noreply.github.com>
2019-05-23 13:32:19 +0430

    Feature/releasenote (#430)
    * add release not layout
    
    * add release not components
    
    * add release note sass, #424
    
    * release note page without API is ready
    
    * remove empty page(Help and Policies)
    
    * adding table material to settings
    
    * add a border in release not layout
    
    * correct label name
    
    * correct path name

---
+++ src/pages/ReleaseNote.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="releaseNote">
+
+    <!-- LEFT COLUMN -->
+
+    <div class="left-column">
+
+      <!-- HEADER -->
+
+      <div class="header">
+        <p class="title">Release Note</p>
+      </div>
+
+      <!-- CONTENT -->
+
+      <div class="content">
+        <div
+          v-for="note in releaseNotes"
+          :key="note.date"
+          class="single-note"
+        >
+          <p class="date">{{ moment(note.date).format('DD MMMM, YYYY') }}</p>
+          <p class="title">{{ note.title }}</p>
+          <p class="description">{{ note.description }}</p>
+        </div>
+
+      </div>
+    </div>
+
+    <!-- RELEASE NOTE FORMS -->
+
+    <div class="right-column">
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+  name: 'ReleaseNote',
+  data () {
+    return {
+      releaseNotes: require('../../static/release-notes.json')
+    }
+  },
+  methods: {
+    moment
+  }
+}
+</script>
