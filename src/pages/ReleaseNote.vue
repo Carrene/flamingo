9f2afec321427733bcd17fe3d41a1dc24b9af2cc
ghazal-khaki <ghazalkhaki3@gmail.com>
2019-05-23 11:56:30 +0430

    release note page without API is ready

--- src/pages/ReleaseNote.vue
+++ src/pages/ReleaseNote.vue
@@ -19,7 +19,7 @@
           :key="note.date"
           class="single-note"
         >
-          <p class="date">{{ note.date }}</p>
+          <p class="date">{{ moment(note.date).format('DD MMMM, YYYY') }}</p>
           <p class="title">{{ note.title }}</p>
           <p class="description">{{ note.description }}</p>
         </div>
@@ -27,108 +27,24 @@
       </div>
     </div>
 
-    <!-- TAG FORMS -->
+    <!-- RELEASE NOTE FORMS -->
 
     <div class="right-column">
-      <!-- <new-release-note-form class="form" /> -->
-      <!--    @created="selectTag"
-        v-if="!selectedTag"
-      /> -->
-      <update-release-note-form class="form" />
-
-      <!--         :selectedTag="selectedTag"
-        @showNewReleaseNoteForm="emptySelectedTag"
-        v-else -->
     </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-// import server from '../server'
-const UpdateReleaseNoteForm = () => import(
-  /* webpackChunkName: "UpdateReleaseNoteForm" */ '../components/UpdateReleaseNoteForm'
-)
-const NewReleaseNoteForm = () => import(
-  /* webpackChunkName: "NewTagForm" */ '../components/NewTagForm'
-)
-
+import moment from 'moment'
 export default {
   name: 'ReleaseNote',
   data () {
     return {
-      releaseNotes: [
-        {
-          title: 'lorem',
-          date: '1/1/1',
-          description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Ipsam eveniet, reprehenderit fugiat deserunt, laboriosam nulla maiores dolor,
-            nostrum voluptatem temporibus amet exercitationem alias possimus quod modi distinctio.
-            Aperiam, harum itaque.`
-        },
-        {
-          title: 'lorem',
-          date: '2/2/2',
-          description: 'lorem lorem'
-        },
-        {
-          title: 'lorem',
-          date: '3/3/3',
-          description: 'lorem lorem'
-        },
-        {
-          title: 'lorem',
-          date: '4/4/4',
-          description: 'lorem lorem'
-        },
-        {
-          title: 'lorem',
-          date: '5/5/5',
-          description: 'lorem lorem'
-        }
-      ]
+      releaseNotes: require('../../static/release-notes.json')
     }
   },
-  computed: {
-    // headers () {
-    //   return [
-    //     {
-    //       label: this.tagMetadata.fields.title.label,
-    //       field: 'tagName',
-    //       className: 'tag-name'
-    //     },
-    //     {
-    //       label: 'Preview',
-    //       field: 'preview',
-    //       className: 'preview'
-    //     },
-    //     {
-    //       label: this.tagMetadata.fields.description.label,
-    //       field: 'tagDescription',
-    //       className: 'tag-description'
-    //     }
-    //   ]
-    // },
-    ...mapState([
-      // 'tags'
-    ])
-  },
   methods: {
-    // emptySelectedTag () {
-    //   this.selectedTag = null
-    // },
-    // selectTag (tag) {
-    //   this.selectedTag = tag
-    // }
-  },
-  components: {
-    UpdateReleaseNoteForm,
-    NewReleaseNoteForm
-  },
-  beforeMount () {
-    // if (this.tags.length) {
-    //   this.selectedTag = this.tags[0]
-    // }
+    moment
   }
 }
 </script>
